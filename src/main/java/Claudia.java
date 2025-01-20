d2e5d9536ef0c407852167cedcf78949ae166a50
feliciahmq <131253731+feliciahmq@users.noreply.github.com>
2025-01-20 22:44:09 +0800

    Add support for list

--- src/main/java/Claudia.java
+++ src/main/java/Claudia.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
 public class Claudia {
+    private static String[] tasks = new String[100];
+    private static int noOfTasks = 0;
+
     public static void main(String[] args) {
-        String greet = "Hello! I'm Claudia.\n" + " What can I do for you?";
+        String greet = " Hello! I'm Claudia.\n What can I do for you?";
         String exit = " Bye. Hope to see you again soon!";
 
         print(greet);
@@ -14,17 +17,37 @@ public class Claudia {
             if (input.equals("bye")) {
                 print(exit);
                 break;
+            } else if (input.equals("list")) {
+                displayList();
+            } else {
+                addTask(input);
             }
-
-            print(input);
         }
 
+        scanner.close();
     }
 
     private static void print(String s) {
         String output = "____________________________________________________________\n"
-                + " " + s + "\n"
+                + s + "\n"
                 + "____________________________________________________________\n";
         System.out.println(output);
+    }
+
+    private static void addTask(String task) {
+        tasks[noOfTasks] = task;
+        noOfTasks++;
+        print(" added: " + task);
+    }
+
+    private static void displayList() {
+        String output = "";
+        for (int i = 0; i < noOfTasks; i++) {
+            output += " " + (i + 1) + ". " + tasks[i];
+            if (i < noOfTasks - 1) {
+                output += "\n";
+            }
+        }
+        print(output);
     }
 }
