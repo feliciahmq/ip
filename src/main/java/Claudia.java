189e460914cf552fc7cb542f0e040ad39dca727a
feliciahmq <131253731+feliciahmq@users.noreply.github.com>
2025-01-20 22:24:47 +0800

    Add support for echo

--- src/main/java/Claudia.java
+++ src/main/java/Claudia.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 public class Claudia {
     public static void main(String[] args) {
+        String greet = "Hello! I'm Claudia.\n" + " What can I do for you?";
+        String exit = " Bye. Hope to see you again soon!";
 
-        String greeting = "____________________________________________________________\n"
-                + " Hello! I'm Claudia.\n"
-                + " What can I do for you?\n"
+        print(greet);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                print(exit);
+                break;
+            }
+
+            print(input);
+        }
+
+    }
+
+    private static void print(String s) {
+        String output = "____________________________________________________________\n"
+                + " " + s + "\n"
                 + "____________________________________________________________\n";
-
-        String exit = " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-
-        System.out.println(greeting + exit);
-
+        System.out.println(output);
     }
 }
