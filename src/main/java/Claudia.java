8af32cbffba1bdfa863c08bf276248fc5d4a9e07
feliciahmq <131253731+feliciahmq@users.noreply.github.com>
2025-01-20 22:06:39 +0800

    Restructure greet and exit

--- src/main/java/Claudia.java
+++ src/main/java/Claudia.java
@@ -1,10 +1,15 @@
 public class Claudia {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Claudia.");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        String greeting = "____________________________________________________________\n"
+                + " Hello! I'm Claudia.\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n";
+
+        String exit = " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+
+        System.out.println(greeting + exit);
+
     }
 }
